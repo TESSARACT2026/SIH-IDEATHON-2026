e21585a17e812be12366402ed830dc851a95de3e
aayush-6753 <aayushkmr825@gmail.com>
2026-08-22 08:37:26 +0530

    chore: stop tracking generated files

--- backend/dist/modules/services/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare const router: import("express-serve-static-core").Router;
-export declare function getHolidays(countryCode: string, year: number): Promise<any>;
-export default router;
-//# sourceMappingURL=index.d.ts.map
