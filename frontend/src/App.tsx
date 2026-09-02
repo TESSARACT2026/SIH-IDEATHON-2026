6bf7d3ae43cacc3c32495d85b73f9ae3af2b3589
arnabsharma44-sketch <arnabsharma44@gmail.com>
2026-09-02 18:30:51 +0530

    feat: add Book Trains page with IRCTC, MakeMyTrip, ConfirmTkt price comparison

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -27,6 +27,7 @@ import { ProfilePage } from './pages/ProfilePage';
 import { GroupPlanPage } from './pages/GroupPlanPage';
 import { PreBookedCabsPage } from './pages/PreBookedCabsPage';
 import { HotelsPage } from './pages/HotelsPage';
+import { TrainsPage } from './pages/TrainsPage';
 import { ProtectedRoute } from './components/layout/ProtectedRoute';
 import { VoiceNav } from './components/ui/VoiceNav';
 const queryClient = new QueryClient({
@@ -131,6 +132,7 @@ function App() {
                 <Route path="/group/:code?" element={<GroupPlanPage />} />
                 <Route path="/cabs" element={<PreBookedCabsPage />} />
                 <Route path="/hotels" element={<HotelsPage />} />
+                <Route path="/trains" element={<TrainsPage />} />
                 <Route path="*" element={<Navigate to="/dashboard" replace />} />
               </Routes>
             </BrowserRouter>
